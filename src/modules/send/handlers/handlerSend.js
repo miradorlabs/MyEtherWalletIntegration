2696546b23fa00b01b50f5c2d08fdd8c59a3e173
Dawson Walker <dawsonwalker91@gmail.com>
2025-12-01 14:11:23 -0700

    Add parallax and handle transaction sending

--- src/modules/send/handlers/handlerSend.js
+++ src/modules/send/handlers/handlerSend.js
@@ -1,4 +1,5 @@
 import { toBN, toHex, toChecksumAddress, isHexStrict } from 'web3-utils';
+import { ParallaxClient } from '@miradorlabs/parallax';
 import { isAddress } from '@/core/helpers/addressUtils';
 import SanitizeHex from '@/core/helpers/sanitizeHex';
 import { Transaction } from '@ethereumjs/tx';
@@ -141,19 +142,103 @@ class SendTransaction {
       .encodeABI();
   }
   async submitTransaction() {
+    // Initialize Parallax client (API key optional)
+    const client = new ParallaxClient();
+    let trace, span;
     try {
+      // Create a trace first
+      trace = await client.createTrace({
+        name: 'SendTransaction',
+        attributes: {
+          from: this.TX.from,
+          to: this.TX.to,
+          value: this.TX.value,
+          destination: this.TX.destination,
+          network: this.network?.name || 'Unknown',
+        },
+        tags: ['transaction', 'wallet']
+      });
+      const traceId = trace.traceId;
+      // Start a span for this transaction
+      span = await client.startSpan({
+        traceId,
+        name: 'submitTransaction',
+        attributes: {
+          from: this.TX.from,
+          to: this.TX.to,
+          value: this.TX.value,
+          destination: this.TX.destination,
+          network: this.network?.name || 'Unknown',
+        }
+      });
+      await client.addSpanEvent({
+        traceId,
+        spanId: span.spanId,
+        eventName: 'prepare_tx',
+        attributes: {
+          destination: this.TX.destination,
+          value: this.TX.destinationValue,
+        }
+      });
       this._setTo();
       this._setValue();
       this._setGasPrice();
+      await client.addSpanEvent({
+        traceId,
+        spanId: span.spanId,
+        eventName: 'set_tx_fields',
+        attributes: {
+          to: this.TX.to,
+          value: this.TX.value,
+          gasPrice: this.TX.gasPrice,
+        }
+      });
       const nonce = await this.web3().eth.getTransactionCount(this.address());
+      await client.addSpanEvent({
+        traceId,
+        spanId: span.spanId,
+        eventName: 'nonce_retrieved',
+        attributes: { nonce }
+      });
       this.setNonce(nonce);
       this.TX.gasLimit = this.TX.gas;
       const _tx = Transaction.fromTxData(this.TX);
       const json = _tx.toJSON(true);
       json.from = this.address();
       json.toDetails = this.TX.toDetails;
-      return this.web3().eth.sendTransaction(json);
+      await client.addSpanEvent({
+        traceId,
+        spanId: span.spanId,
+        eventName: 'transaction_ready',
+        attributes: { json }
+      });
+      const result = await this.web3().eth.sendTransaction(json);
+      await client.addSpanEvent({
+        traceId,
+        spanId: span.spanId,
+        eventName: 'transaction_sent',
+        attributes: { txHash: result?.transactionHash }
+      });
+      await client.finishSpan({
+        traceId,
+        spanId: span.spanId,
+        status: 'success'
+      });
+      return result;
     } catch (e) {
+      if (trace && span && span.spanId) {
+        await client.addSpanEvent({
+          traceId: trace.traceId,
+          spanId: span.spanId,
+          eventName: 'error',
+          attributes: { message: e?.message || e }
+        });
+        await client.finishSpan({
+          traceId: trace.traceId,
+          spanId: span.spanId,
+          status: 'error'
+        });
+      }
       return e;
     }
   }
